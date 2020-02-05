e3aa08408898f9e2c647832608a15e1f9886488b
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-02-05 21:55:15 +0100

    api model class for filtering

---
+++ app/src/main/java/com/cosmic/venten/model/api_model.kt
@@ -0,0 +1,12 @@
+package com.cosmic.venten.model
+
+class api_model() {
+
+    val id: Int? = null
+    val start_year: Int? = null
+    val end_year: Int? = null
+    val gender: String? = null
+    val countries: ArrayList<String>? = null
+    val colors: ArrayList<String>? = null
+
+}
