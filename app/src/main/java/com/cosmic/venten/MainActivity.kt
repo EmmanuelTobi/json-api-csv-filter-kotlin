177490cd93d6603700787f1a14c7a0866cc8cd34
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-02-06 22:16:06 +0100

    MainActivity with api url access

--- app/src/main/java/com/cosmic/venten/MainActivity.kt
+++ app/src/main/java/com/cosmic/venten/MainActivity.kt
@@ -1,39 +1,98 @@
 package com.cosmic.venten
 
+import android.app.Activity
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import org.json.JSONArray
+import org.json.JSONException
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val mActivity: Activity = this@MainActivity
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val apiTask = ApiTask()
+        apiTask.execute()
+
+    }
+
+    class ApiTask: AsyncTask<Void, Void, String>() {
+        override fun doInBackground(vararg params: Void?): String? {
+
+            val url = "https://ven10.co/assessment/filter.json"
+            var result = ""
+
+            try {
+
+                val Url = URL(url)
+                val connection = Url.openConnection() as HttpURLConnection
+                val `is` = connection.inputStream
+                val br = BufferedReader(InputStreamReader(`is`, "utf-8"), 8)
+                val sBuilder = StringBuilder()
+
+                var line = br.readLine()
+                sBuilder.append(line + "\n")
+
+                while (line != null) {
+
+                    line = br.readLine()
+                    sBuilder.append(line + "\n")
+                }
+
+                `is`.close()
+                result = sBuilder.toString()
+
+            } catch (e: Exception) {
+
+                e.printStackTrace()
+            }
+
+            return result
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        override fun onPreExecute() {
+            super.onPreExecute()
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            Log.i("apiTask", result.toString())
+
+            try {
+
+                val jArray = JSONArray(result)
+                var i = 0
+                while (i < jArray.length()) {
+
+                    val jObject = jArray.getJSONObject(i)
+
+                    val id = jObject.getString("id")
+
+                    Log.i("JsonID", id)
+
+                    i++
+                }
+
+                Log.i("JsonLength", i.toString())
+
+            } catch (e: JSONException) {
+                Log.e("JSONException", "Error: " + e.toString())
+            }
         }
     }
 }
