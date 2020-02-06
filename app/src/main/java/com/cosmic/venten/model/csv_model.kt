e2184e62cc933c0cfda51f88fa6a447679d467e7
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-02-06 10:58:34 +0100

    csv model class for adapter

---
+++ app/src/main/java/com/cosmic/venten/model/csv_model.kt
@@ -0,0 +1,16 @@
+package com.cosmic.venten.model
+
+class csv_model() {
+
+    val id: Int? = null
+    val first_name: String? = null
+    val last_name: String? = null
+    val email: String? = null
+    val country: String? = null
+    val car_model: String? = null
+    val car_model_year: Int? = null
+    val gender: String? = null
+    val job_title: String? = null
+    val car_color: String? = null
+    val bio: String? = null
+}
